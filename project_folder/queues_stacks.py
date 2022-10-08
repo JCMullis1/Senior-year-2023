c9387a8702502ba5d615f9b044c61943923e3d5d
JCMullis1 <joncalebmullis@gmail.com>
2022-10-08 18:03:52 -0500

    Never gonna give you up never gonna let you down

--- project_folder/queues_stacks.py
+++ project_folder/queues_stacks.py
@@ -18,5 +18,35 @@ def edit(stack):
         edit(stack)
     return stack
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-edit(arr)
+
+    def insert(self, new_value):
+        if self.next is None:
+            self.next = Node(new_value)
+            return
+
+        self.next.insert(new_value)
+
+    def show(self):
+        print(self.value)
+        if self.next is not None:
+            self.next.show()
+
+    def stack(self):
+        pass
+
+    def queue(self):
+        pass
+
+
+# edit(arr)
+
+firstNode = Node(1)
+firstNode.insert(2)
+firstNode.insert(3)
+firstNode.insert(5)
+firstNode.show()
